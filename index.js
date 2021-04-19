9e7391f6bf8b694ca73b4033fd730a3f0d4b2b2f
Tugay Serinken <tugay.serinken@hotmail.de>
2021-04-20 04:37:46 +0700

    add appId

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express')
 const proxy = require('express-http-proxy')
 const cors = require('cors')
 
+const appId = 'WandoInt-217b-42d8-a699-e79808dd505e'
 const app = express()
 app.use(cors())
-app.use('/', proxy('https://svcs.sandbox.ebay.com'))
+app.use(
+  proxy('https://svcs.sandbox.ebay.com', {
+    proxyReqPathResolver: function (req) {
+      const parts = req.url.split('?')
+      const queryString = parts[1]
+      const updatedPath = parts[0]
+      return (
+        updatedPath +
+        ('?SECURITY-APPNAME=' + appId) +
+        (queryString ? '&' + queryString : '')
+      )
+    },
+  })
+)
 app.listen(4000, () => console.log('Ebay Forwarder listening on port 4000!'))
