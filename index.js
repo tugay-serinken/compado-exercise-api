7dd323dfd640123a5d7e3d0c1698274a98eca08a
Tugay Serinken <tugay.serinken@hotmail.de>
2021-04-19 19:19:43 +0700

    implement forwarder

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use('/', proxy('https://svcs.sandbox.ebay.com'))
+app.listen(4000, () => console.log('Ebay Forwarder listening on port 4000!'))
