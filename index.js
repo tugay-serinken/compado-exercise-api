2e3c20e7302f5146d52b169fad2ef67e26774f1c
tugayserinken <tugay.serinken@compado.com>
2021-05-23 03:13:15 +0700

    change API

--- index.js
+++ index.js
@@ -5,18 +5,5 @@ const cors = require('cors')
 const appId = 'WandoInt-217b-42d8-a699-e79808dd505e'
 const app = express()
 app.use(cors())
-app.use(
-  proxy('https://svcs.sandbox.ebay.com', {
-    proxyReqPathResolver: function (req) {
-      const parts = req.url.split('?')
-      const queryString = parts[1]
-      const updatedPath = parts[0]
-      return (
-        updatedPath +
-        ('?SECURITY-APPNAME=' + appId) +
-        (queryString ? '&' + queryString : '')
-      )
-    },
-  })
-)
-app.listen(4000, () => console.log('Ebay Forwarder listening on port 4000!'))
+app.use(proxy('https://jsonplaceholder.typicode.com'))
+app.listen(4000, () => console.log('API Forwarder listening on port 4000!'))
